fa9507c187d04dba1dc93ebe51f9752232c6d7b7
Alejandro-17 <diego.osorio2@udea.edu.co>
2021-09-18 23:52:48 -0500

    Empezo a tronar entonces apago pc y trabajo en algoritmo

--- main.cpp
+++ main.cpp
@@ -10,5 +10,10 @@ int main()
     string filename = "../Parcial2/images/colombia.jpg";
     QImage im(filename.c_str());
 
+    unsigned int pixelX=200;
+    unsigned int pixelY=200;
+    cout<<"Intensidad de color rojo del pixel seleccionado es: "<< im.pixelColor(pixelX,pixelY).red()<<endl;
+
+
     return 0;
 }
