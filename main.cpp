0c393050165514bb9491e01e7036a9370c523867
Alejandro-17 <diego.osorio2@udea.edu.co>
2021-09-19 18:54:44 -0500

    Base para leer imagenes e imprimir las intensidades de cada pixel

--- main.cpp
+++ main.cpp
@@ -13,7 +13,14 @@ int main()
     unsigned int pixelX=200;
     unsigned int pixelY=200;
     cout<<"Intensidad de color rojo del pixel seleccionado es: "<< im.pixelColor(pixelX,pixelY).red()<<endl;
+    cout<<"Intensidad de color verde del pixel seleccionado es: "<< im.pixelColor(pixelX,pixelY).green()<<endl;
+    cout<<"Intensidad de color azul del pixel seleccionado es: "<< im.pixelColor(pixelX,pixelY).blue()<<endl;
 
+   for (int indx=0; indx<im.width() ; ++indx){
+       for(int indy=0; indy<im.height(); ++indy){
+           cout<< indx << ","<< indy << "="<<im.pixelColor(indx,indy).red()<<endl;
+       }
+   }
 
     return 0;
 }
