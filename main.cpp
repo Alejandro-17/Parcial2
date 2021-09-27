463b23aa2d104254befe91c00fabe95d31ce9709
Alejandro-17 <diego.osorio2@udea.edu.co>
2021-09-26 20:07:23 -0500

    Avance codigo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <QImage>
+#include <vector>
+#include <QColor>
 #include <string>
 
 using namespace std;
@@ -7,20 +9,39 @@ using namespace std;
 
 int main()
 {
-    string filename = "../Parcial2/images/colom.jpg";
+    string filename = "../Parcial2/images/brasil.jpg";
     QImage im(filename.c_str());
-
+/*
     unsigned int pixelX=200;
     unsigned int pixelY=200;
     cout<<"Intensidad de color rojo del pixel seleccionado es: "<< im.pixelColor(pixelX,pixelY).red()<<endl;
     cout<<"Intensidad de color verde del pixel seleccionado es: "<< im.pixelColor(pixelX,pixelY).green()<<endl;
     cout<<"Intensidad de color azul del pixel seleccionado es: "<< im.pixelColor(pixelX,pixelY).blue()<<endl;
+    cout<<"Formato de la imagen es: "<< im.format()<<endl;
+*/
+    ///////////////////////////////////////////////////////////////// Adelanto
+       vector <QColor> pix1;
+       int limiteX=0,limiteY=0;
+       short int Rpromedio=0, Gpromedio=0, Bpromedio=0;
+       while(limiteX < im.width() && limiteY< im.height()){
+       int sumR=0,sumG=0,sumB=0;
+       for (int indx=limiteX; indx<limiteX+4 ; ++indx){
+       for(int indy=0; indy<4; ++indy){
+        sumR= sumR + im.pixelColor(indx,indy).red();
+        sumG = sumG + im.pixelColor(indx,indy).green();
+        sumB= sumB + im.pixelColor(indx,indy).blue();
+      }
+    }
+       Rpromedio= sumR / 16;
+       Gpromedio= sumG / 16;
+       Bpromedio= sumB /16;
+       pix1.push_back(QColor(Rpromedio,Gpromedio,Bpromedio));
+       limiteX+=4;
 
-  // for (int indx=0; indx<im.width() ; ++indx){
-    //   for(int indy=0; indy<im.height(); ++indy){
-     //      cout<< indx << ","<< indy << "="<<im.pixelColor(indx,indy).red()<<endl;
+      // for(auto var:pix1){
+        //   cout<<var.green()<<endl;
       // }
-   //}
- // termino algortimo
+       //cout<<"La capacidad del vector es: "<< pix1.capacity()<<endl;
+        }
     return 0;
 }
